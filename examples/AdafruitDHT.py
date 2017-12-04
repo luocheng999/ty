25206b22c842ff48ff1df577ac88e28ff476446f
luocheng999 <e1234e129@gmail.com>
2017-12-04 16:35:26 +0800

    04052492 c3

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -22,7 +22,30 @@
 import sys
 
 import Adafruit_DHT
-
+import time
+import httplib, urllib
+import json
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+deviceId="DNP2KUBy"
+deviceKey="O9azxaA6wEEku7dV"
+def post_to_mcs(payload):
+	headers={"Content-type":"application/json","deviceKey": deviceKey}
+	not_connected=1
+	while(not_connected):
+		try:
+			conn=httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect()
+			not_connected=0
+		except(httplib.HTTPException,socket.error)as ex:
+			print "Error: %s"
+			# sleep 10 seconds
+	conn.request("POST","/mcs/v2/devices/" + deviceId + "/datapoints",json.dumps(payload),headers)
+	response=conn.getresponse()
+	print(response.status,response.reason,json.dumps(payload),time.strftime("%c"))
+	data=response.read()
+	conn.close()
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
@@ -48,9 +71,22 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 while True:
+	SwitchStatus=GPIO.input(17)
 	humidity, temperature=Adafruit_DHT.read_retry(11,4)
+	h0,t0=Adafruit_DHT.read_retry(11,4)
 	if humidity is not None and temperature is not None:
     		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+			  {"dataChnId":"Temperature","values":{"value":t0}},
+			 {"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
+		post_to_mcs(payload)
+		time.sleep(1)
 	else:
     		print('Failed to get reading. Try again!')
     		sys.exit(1)
+	
+	if(SwitchStatus==0):
+		print('button pressed')
+	else:
+		print('button released')
+	
