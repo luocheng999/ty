65807dd04fadfdb363a302290b55f4762ca6f07e
luocheng999 <e1234e129@gmail.com>
2017-12-11 13:36:54 +0800

    p3

--- examples/AdafruitDHT04052492.py
+++ examples/AdafruitDHT04052492.py
@@ -20,32 +20,33 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import sys
-
-import Adafruit_DHT
 import time
 import httplib, urllib
 import json
+import Adafruit_DHT
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-deviceId="DNP2KUBy"
-deviceKey="O9azxaA6wEEku7dV"
-def post_to_mcs(payload):
-	headers={"Content-type":"application/json","deviceKey": deviceKey}
-	not_connected=1
-	while(not_connected):
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+deviceId = "DNP2KUBy"
+deviceKey = "O9azxaa6wEEku7dv" 
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
 		try:
-			conn=httplib.HTTPConnection("api.mediatek.com:80")
-			conn.connect()
-			not_connected=0
-		except(httplib.HTTPException,socket.error)as ex:
-			print "Error: %s"
-			# sleep 10 seconds
-	conn.request("POST","/mcs/v2/devices/" + deviceId + "/datapoints",json.dumps(payload),headers)
-	response=conn.getresponse()
-	print(response.status,response.reason,json.dumps(payload),time.strftime("%c"))
-	data=response.read()
-	conn.close()
+			conn = httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (httplib.HTTPException, socket.error) as ex: 
+			print "Error: %s" % ex
+			time.sleep(10) # sleep 10 seconds
+ 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
+
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
@@ -61,33 +62,33 @@ else:
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+#humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
 
-# Note that sometimes you won't get a reading and
+
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 while True:
-	SwitchStatus=GPIO.input(17)
-	humidity, temperature=Adafruit_DHT.read_retry(11,4)
-	h0,t0=Adafruit_DHT.read_retry(11,4)
+	
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	SwitchStatus = GPIO.input(17)
+	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
 	if humidity is not None and temperature is not None:
-    		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+   		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
 			  {"dataChnId":"Temperature","values":{"value":t0}},
 			 {"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
 		post_to_mcs(payload)
 		time.sleep(1)
 	else:
-    		print('Failed to get reading. Try again!')
+   		print('Failed to get reading. Try again!')
     		sys.exit(1)
-	
-	if(SwitchStatus==0):
-		print('Button Pressed')
-	else:
-		print('Button Released')
 
-	
+
+	if( SwitchStatus == 0):
+		print('button pressed')
+	else:
+		print('button released')
--- examples/AdafruitDHT1.py
+++ examples/AdafruitDHT1.py
@@ -77,17 +77,18 @@ while True:
 	if humidity is not None and temperature is not None:
     		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-			  {"dataChnId":"Temperature","values":{"value":t0}},
-			 {"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
+			  {"dataChnId":"Temperature","values":{"value":t0}}]}
 		post_to_mcs(payload)
-		time.sleep(1)
+		time.sleep(10)
 	else:
     		print('Failed to get reading. Try again!')
     		sys.exit(1)
-	
+
 	if(SwitchStatus==0):
 		print('Button Pressed')
 	else:
 		print('Button Released')
+	
 
 	
+
